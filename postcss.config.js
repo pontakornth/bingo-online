94c0bd251e4fb1bb32430e39bf966140551fb53a
pontakornth <pontakorn61@gmail.com>
2020-09-15 16:36:07 +0700

    chore: Setup Purgecss and autoprefixer

---
+++ postcss.config.js
@@ -0,0 +1,19 @@
+/* eslint-disable global-require */
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable @typescript-eslint/no-var-requires */
+const IN_PRODUCTION = process.env.NODE_ENV === 'production';
+
+module.exports = {
+  plugins: [
+    require('autoprefixer')(),
+    IN_PRODUCTION && require('@fullhuman/postcss-purgecss')({
+      content: ['./public/**/*.html', './src/**/*.vue'],
+      defaultExtractor(content) {
+        const contentWithoutStyleBlocks = content.replace(/<style[^]+?<\/style>/gi, '');
+        return contentWithoutStyleBlocks.match(/[A-Za-z0-9-_/:]*[A-Za-z0-9-_/]+/g) || [];
+      },
+      whitelist: [],
+      whitelistPatterns: [/-(leave|enter|appear)(|-(to|from|active))$/, /^(?!(|.*?:)cursor-move).+-move$/, /^router-link(|-exact)-active$/, /data-v-.*/],
+    }),
+  ],
+};
